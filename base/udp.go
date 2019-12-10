3797a51d298650fb3f79609b762ca5e45890f927
Gaetan Crahay <gaetan@crahay.eu>
2019-12-10 12:37:28 +0100

    Debug logging in dhcp

--- base/udp.go
+++ base/udp.go
@@ -5,7 +5,7 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/krolaw/dhcp4"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net"
 )
 
@@ -45,23 +45,28 @@ func Serve(in chan gopacket.Packet, out chan []byte, handler dhcp4.Handler) erro
 		packet := <-in
 		dhcpPacket, err := preparePacket(packet)
 		if err != nil {
+			log.Printf("DHCP: Cannot prepare packet: %v", err)
 			continue
 		}
 		n := dhcpPacket.UDP.Length
 		if n < 240 { // Packet too small to be DHCP
+			log.Printf("DHCP: Packet too small: %d", n)
 			continue
 		}
 		req := dhcp4.Packet(dhcpPacket.UDP.Payload)
 		if req.HLen() > 16 { // Invalid size
+			log.Printf("DHCP: Payload too small: %d", req.HLen())
 			continue
 		}
 		options := req.ParseOptions()
 		var reqType dhcp4.MessageType
 		if t := options[dhcp4.OptionDHCPMessageType]; len(t) != 1 {
+			log.Printf("DHCP: No or too much DHCP message types %d", len(t))
 			continue
 		} else {
 			reqType = dhcp4.MessageType(t[0])
 			if reqType < dhcp4.Discover || reqType > dhcp4.Inform {
+				log.Printf("DHCP: Wrong message type: %+v", reqType)
 				continue
 			}
 		}
@@ -82,6 +87,7 @@ func Serve(in chan gopacket.Packet, out chan []byte, handler dhcp4.Handler) erro
 			conf := *handler.(*Server)
 			device, found := conf.GetDevice(dhcpPacket.Ethernet.SrcMAC.String())
 			if !found {
+				log.Printf("Destination device not found: %s", dhcpPacket.Ethernet.SrcMAC.String())
 				continue
 			}
 			var ip *layers.IPv4
@@ -119,6 +125,7 @@ func Serve(in chan gopacket.Packet, out chan []byte, handler dhcp4.Handler) erro
 				log.Printf("Cannot serialize response: %v", err)
 				continue
 			}
+			log.Printf("Sending DHCP response %+v", ip)
 			out <- buffer.Bytes()
 		}
 	}
