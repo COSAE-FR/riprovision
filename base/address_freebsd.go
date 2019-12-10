9d8c8b6334a52a8be101b22231f57ed21efa5f85
Gaetan Crahay <gaetan@crahay.eu>
2019-12-10 11:36:07 +0100

    add ifconfig on freebsd

--- base/address_freebsd.go
+++ base/address_freebsd.go
@@ -12,12 +12,18 @@ func AddInterfaceIP(ip net.IP, mask net.IPMask, iface string) error {
 	msk := net.IPv4(mask[0], mask[1], mask[2], mask[3]).String()
 	log.Printf("Adding interface address: ifconfig %s %s netmask %s alias", iface, ip.String(), msk)
 	cmd := exec.Command("ifconfig", iface, ip.String(), "netmask", msk, "alias")
-	return cmd.Run()
+	out, err := cmd.CombinedOutput()
+	log.Printf("CMD outputs error: %v", err)
+	log.Printf("Out:\n%s", string(out))
+	return err
 }
 
 func RemoveInterfaceIP(ip net.IP, mask net.IPMask, iface string) error {
 	msk := net.IPv4(mask[0], mask[1], mask[2], mask[3]).String()
 	log.Printf("Removing interface address: ifconfig %s %s netmask %s delete", iface, ip.String(), msk)
 	cmd := exec.Command("ifconfig", iface, ip.String(), "netmask",msk, "delete")
-	return cmd.Run()
+	out, err := cmd.CombinedOutput()
+	log.Printf("CMD outputs error: %v", err)
+	log.Printf("Out:\n%s", string(out))
+	return err
 }
