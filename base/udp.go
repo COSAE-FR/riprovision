8af52ce5e590aee0644f172ed6e53b1d35f58146
Gaetan Crahay <gaetan@crahay.eu>
2019-12-10 12:29:03 +0100

    DHCP responses

--- base/udp.go
+++ base/udp.go
@@ -5,16 +5,23 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/krolaw/dhcp4"
+	"log"
+	"net"
 )
 
 type DHCPPacket struct {
 	Packet gopacket.Packet
+	Ethernet *layers.Ethernet
 	IP     *layers.IPv4
 	UDP    *layers.UDP
 }
 
 func preparePacket(packet gopacket.Packet) (*DHCPPacket, error) {
 	pckt := &DHCPPacket{Packet: packet}
+	ethLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethLayer == nil {
+		return pckt, errors.New("not an Ethernet packet")
+	}
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 	if ipLayer == nil {
 		return pckt, errors.New("not an IP packet")
@@ -23,13 +30,17 @@ func preparePacket(packet gopacket.Packet) (*DHCPPacket, error) {
 	if udpLayer == nil {
 		return pckt, errors.New("not a UDP packet")
 	}
+	pckt.Ethernet = ethLayer.(*layers.Ethernet)
 	pckt.IP = ipLayer.(*layers.IPv4)
 	pckt.UDP = udpLayer.(*layers.UDP)
 	return pckt, nil
 }
 
 func Serve(in chan gopacket.Packet, out chan []byte, handler dhcp4.Handler) error {
-
+	packetOptions := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
 	for {
 		packet := <-in
 		dhcpPacket, err := preparePacket(packet)
@@ -68,6 +79,47 @@ func Serve(in chan gopacket.Packet, out chan []byte, handler dhcp4.Handler) erro
 			if _, e := conn.WriteTo(res, addr); e != nil {
 				return e
 			} */
+			conf := *handler.(*Server)
+			device, found := conf.GetDevice(dhcpPacket.Ethernet.SrcMAC.String())
+			if !found {
+				continue
+			}
+			var ip *layers.IPv4
+			if dhcpPacket.IP.SrcIP.Equal(net.IPv4zero) || dhcpPacket.IP.DstIP.Equal(net.IPv4bcast) {
+				ip = &layers.IPv4{
+					SrcIP:    *device.DHCP.ServerIP,
+					DstIP:    net.IPv4bcast,
+					Protocol: layers.IPProtocolUDP,
+				}
+			} else {
+				ip = &layers.IPv4{
+					SrcIP:    *device.DHCP.ServerIP,
+					DstIP:   *device.DHCP.ClientIP,
+					Protocol: layers.IPProtocolUDP,
+					Version: 4,
+					TTL: 64,
+				}
+			}
+			udp := &layers.UDP{
+				SrcPort: layers.UDPPort(67),
+				DstPort: layers.UDPPort(68),
+			}
+			udp.Payload = res
+			if err := udp.SetNetworkLayerForChecksum(ip); err != nil {
+				log.Printf("Cannot set network layer: %v", err)
+				continue
+			}
+			eth := &layers.Ethernet{
+				SrcMAC:       conf.Iface.HardwareAddr,
+				DstMAC:       dhcpPacket.Ethernet.SrcMAC,
+				EthernetType: layers.EthernetTypeIPv4,
+			}
+			buffer := gopacket.NewSerializeBuffer()
+			if err := gopacket.SerializeLayers(buffer, packetOptions, eth, ip, udp); err != nil {
+				log.Printf("Cannot serialize response: %v", err)
+				continue
+			}
+			out <- buffer.Bytes()
 		}
 	}
 }
